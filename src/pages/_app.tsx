c06fa9f93fecacd19df08a5a4b7d509f1a864f85
Awais <awais@southvillesol.com>
2023-02-07 18:49:28 +0500

    integrate react-ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {useEffect} from "react";
+import ReactGA from "react-ga"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const TRACKING_ID = "G-MN1E5QZBMW"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+export default function App({Component, pageProps}: AppProps) {
+    useEffect(() => {
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }, []);
+    return <Component {...pageProps} />
 }
